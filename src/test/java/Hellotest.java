e11831b44973562591b893516ba5190e93eded13
laviniabutilchin <laviniabutilchin@gmail.com>
2021-12-18 00:18:24 +0000

    added started hello feature

--- src/test/java/Hellotest.java
+++ src/test/java/Hellotest.java
@@ -3,5 +3,7 @@ public class Hellotest {
     public static void main(String[] args) {
         System.out.println("hellotest");
         System.out.println("Developing hello feature");
+        System.out.println("GitHub Reto is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
